8f3a3b98ec7ddf9d01117fb48c8afa5b47863a6f
Emil <e.garaev.dg@gmail.com>
2025-01-11 14:03:53 +0200

    description update

--- src/components/Playground/components/Description /Description.tsx
+++ src/components/Playground/components/Description /Description.tsx
@@ -5,11 +5,13 @@ const Description: React.FC = () => {
     <div>
       <TypographyHeader>↑ ↓ → ← Arrow-game description</TypographyHeader>
       <TypographyText>
-          The player's task is to press the arrow key shown to him on the keyboard until the next one appears.
+        The player's task is to press the arrow key shown to him on the keyboard
+        until the next one appears.
       </TypographyText>
       <TypographyText>
-          After three successful hits in a row - the game is won, after three mistakes - lost. <br/>
-          <b>Good luck Broski!</b>
+        After three successful hits in a row - the game is won, after three
+        mistakes - lost. <br />
+        <b>Good luck Broski!</b>
       </TypographyText>
     </div>
   );
